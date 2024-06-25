7d591f2c45937e14b08c4619c2d16e47ee65a75c
JoseDev <joseijd69@gmail.com>
2024-06-25 17:16:39 -0500

    remover middlware

--- app/output/router.py
+++ app/output/router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 
 from app.core.repositories.cache_firestore_repo_impl import CacheQueriesRepo
 from app.logic.query_generator_ai.index import QueryGeneratorAIUC
-from security.auth_validator import verify_auth
+
 
 order_assigner_router = APIRouter()
 
@@ -17,7 +17,7 @@ async def get_data(query: dict):
 
 
 @order_assigner_router.post("/mark_as_good")
-async def get_data(query: dict, _=Depends(verify_auth)):
+async def get_data(query: dict):
     hash = query.get("hash")
     cache_queries_repo = inject.instance(CacheQueriesRepo)
     response = await cache_queries_repo.set_is_valid(hash)
